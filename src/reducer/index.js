e09a830244fd237a795cbdd7818ad830e41e1800
tektechnologies <info@tektechnologies.com>
2018-10-16 17:35:25 -0500

    add test for dashboard

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 
 import auth from './auth';
 import game from './game';
-import dashboard from './dashboard';
+import dashboard from './dashboard-reducer';
 
 export default combineReducers({
   auth,
