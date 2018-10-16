acdc7e7735eb93821768ad673140c237822621ec
Dylan Schroeder <schrodac@gmail.com>
2018-10-16 16:33:09 -0500

    add scaffolding for create game (no functionality)

---
+++ src/components/create-game.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CreateGame extends Component {
+  render() {
+    return (
+      <p> You will create new games here </p>
+    );
+  }
+}
+
+export default (CreateGame);
