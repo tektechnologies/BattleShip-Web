9c817aa5e0802cbedaf8be481d6f52eff4fa3dcb
JohnMV13 <thejohnmiesterV@gmail.com>
2018-10-16 11:43:12 -0500

    work on modal. Pushing so that way everyone can render the home page

--- src/header/modal.js
+++ src/header/modal.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Auth from '../components/auth';
 
 export default class Modal extends Component {
   render() {
@@ -8,8 +7,10 @@ export default class Modal extends Component {
     return (
       <div className={modalName}>
         <main>
-          <Auth/>
-          <button onClick={this.props.handleClose} className='cancel'>Cancel</button>
+          {this.props.title &&
+            <h2>{this.props.title}</h2>}
+          
+          {this.props.children}
         </main>
       </div>
     );
--- src/header/nav.js
+++ src/header/nav.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Modal from './modal';
+import Auth from '../components/auth';
 
 export default class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showAuth: false };
+  }
+  
+  showModal = () => {
+    this.setState({showAuth: true })
+  }
+
+  hideModal = () => {
+    this.setState({showAuth: false,})
+  }
+
   render() {
     return (
       <header className="nav">
@@ -10,7 +24,12 @@ export default class Nav extends Component {
           <Link to='/'><h1>Battle Ship Delta</h1></Link>
           <Link to='/'>Insert Dashboard Link Here</Link> { }
           <Link to='/'>Insert New Game Link Here</Link> { }
-          <button className='signin' onClick={this.showModal}>Sign In Here</button> { }
+          <button className='signin' onClick={this.showModal}>Sign In Here</button>
+          <Modal title='auth' show={this.state.showAuth} handleClose={this.hideModal}>
+            <Auth/>
+            <button className='cancel' onClick={this.hideModal}>Cancel</button>
+          </Modal>
+           { }
           <button className='signup'>Sign Up Here</button>
         </nav>
       </header>
