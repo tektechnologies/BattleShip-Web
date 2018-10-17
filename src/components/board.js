4444216ea9d55fdf6ca9ee33d3fde2b770fa0b40
‘Nathan <ncash13@hotmail.com>
2018-10-17 09:51:41 -0500

    changed to reflect changes in the api

--- src/components/board.js
+++ src/components/board.js
@@ -4,8 +4,13 @@ import {connect} from 'react-redux';
 class Board extends React.Component{
   render(){
     let taken = [];
-    let userShots = [];
-    let opponentShots = [];
+    this.props.shipStatuses.forEach(ship =>{
+      ship.coordinates.forEach(coor =>{
+        taken.push(coor);
+      });
+    });
+    let userShots = this.props.userShots;
+    let opponentShots = this.props.opponentShots;
     
     let rows = ['a', 'b', 'c', 'd', 'e'];
     
@@ -27,9 +32,10 @@ class Board extends React.Component{
               colClass += {i}; //changes column class to col-1 through col-5
               coord = `${row}${i}`; //changes coordinate based on cell instance
               //key++; //key for divs (May or may not be neccessary)
-              if(userShots.includes(coord)){
-                className += 'shot'; //indicates the cell has been targeted by user
-                //needs hit indicator for opponent's ships
+              if(userShots.coord){
+                className += 'hit'; //indicates the cell has been hit/missed by user
+              } else if(userShots.coord === false){
+                className += 'missed';
               }
               className += {rowClass};
               className += {colClass};// className looks like this: 'cell (shot) row-(a-e) col-(1-5)'
@@ -48,18 +54,16 @@ class Board extends React.Component{
               colClass += {i}; //changes column class to col-1 through col-5
               coord = `${row}${i}`;
               //key++;
-              if(opponentShots.includes(coord)){
-                if(taken.includes(coord)){
-                  className += 'hit';
-                } else {
-                  className += 'missed';
-                }
-              }
+              if(opponentShots.coord){
+                className += 'hit';
+              } else if(opponentShots.coord === false) {
+                className += 'missed';
+              }             
               if(taken.includes(coord)){
                 className += 'taken';
               }
               className += {rowClass};
-              className += {colClass};// className looks like this: 'cell (shot) row-(a-e) col-(1-5)'
+              className += {colClass};// className looks like this: 'cell (taken, hit, missed) row-(a-e) col-(1-5)'
               <div key={coord} className={className}></div>;
             }
           })}
