700dd3ae69de13f6997889531d36f80ceea4afb1
Dylan Schroeder <schrodac@gmail.com>
2018-10-17 15:48:27 -0500

    naming consistency for gameList

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,10 +2,10 @@ import { combineReducers } from 'redux';
 
 import auth from './auth-reducer';
 import game from './game';
-import dashboard from './dashboard-reducer';
+import gameList from './dashboard-reducer';
 
 export default combineReducers({
   auth,
   game,
-  dashboard,
+  gameList,
 });
