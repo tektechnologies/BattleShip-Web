d9bcaa5e503c1bc9d703e5d9fe0fd74a90e0a346
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-10-19 11:13:30 -0500

    combined user-reducer with other redux reducers

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import auth from './auth-reducer';
 import game from './game';
 import gameList from './dashboard-reducer';
+import user from './user-reducer';
 
 export default combineReducers({
   auth,
   game,
   gameList,
+  user,
 });
