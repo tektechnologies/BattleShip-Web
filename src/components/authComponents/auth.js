6d96bf0246629d11062659e7016e88940d6dc897
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-10-18 21:51:58 -0500

    moved auth components into their own folder

--- src/components/authComponents/auth.js
+++ src/components/authComponents/auth.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import * as actions from '../actions/auth-actions';
+import * as actions from '../../actions/auth-actions';
 import AuthForm from './auth-form';
 import { withRouter } from 'react-router-dom';
 
