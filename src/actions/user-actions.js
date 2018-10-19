f53e2db9ce4e24205f5f1b439b91f13e94904525
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-10-19 11:12:51 -0500

    set up userFetch: grabs user data and userSet: adds it to redux state

---
+++ src/actions/user-actions.js
@@ -0,0 +1,17 @@
+import superagent from 'superagent';
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+export const userSet = (user) => ({
+  type: 'USER_SET', 
+  payload: user,
+});
+
+export const userFetch = () => (dispatch, getState) => {
+  superagent.get(`${API_URL}/api/games`)
+    .set('Authorization', getState().auth ? `Bearer ${getState().auth}` : null)
+    .then(res => {
+      dispatch(userSet(res.body));
+      return res;
+    });
+};
